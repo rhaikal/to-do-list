08073e2157bc14024dce1597ae71adee204f1aec
rhaikal <rhaikal101@gmail.com>
2022-10-21 19:33:02 +0700

    menambahkan dan mengkonfigurasi library jwt

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
