74a7d7cb90b237371059aa9e29268a62cbd8f4c8
rhaikal <rhaikal101@gmail.com>
2022-10-26 11:27:46 +0700

    membuat model dan migration todo

---
+++ app/Models/Todo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Todo extends Model
+{
+    use HasFactory;
+}
